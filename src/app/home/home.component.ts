7530d0dc9ea5da5e1c54f8f65c660d767887a26f
vasukarthikayini <karthikayini.sv@10decoders.in>
2022-08-16 19:02:40 +0530

    local storage data and get api data

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,19 +6,38 @@ import { CommonService } from '../common.service';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent  {
+  showFlage = false;
   arrData : any
-
-  constructor(private dataService : CommonService) { }
-
-  ngOnInit(): void {
+ constructor(private dataService : CommonService) {}
+ showStyle(){
+  
+  this.showFlage = true
+}
+  ngOnInit(): void{
     this.dataService.getMyAllData().subscribe(response => {
-      console.log('inside the data', response)
+     
       this.arrData = response
-    }
-      )
-    
-    }
+      console.log('inside the data', this.arrData )
+    })
   }
+}
+
+  
+
+ 
+
+ 
+    //this.dataService.getMyAllData().subscribe(response => {
+      //console.log('inside the data', response)
+      //this.arrData = response
+    //})
+      //showStyle(){
+        //console.log("check", this.value);
+//arrData : any
+      //}
+    
+
+  //}
 
 
