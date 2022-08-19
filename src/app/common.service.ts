0e5be3024a5df1a793230cf8dea1a6c3d7bb92ce
vasukarthikayini <karthikayini.sv@10decoders.in>
2022-08-19 14:10:31 +0530

    first commit

--- src/app/common.service.ts
+++ src/app/common.service.ts
@@ -1,21 +1,35 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { BASE_URL } from './utils/constant'
+import { BASE_URL } from './utils/constant';
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CommonService {
+  constructor(private http: HttpClient) {}
 
-
-  constructor(private http: HttpClient) {
-
-   } 
-   
-   ngOnInit() {
-
-   }
-   getMyAllData() {
-    return this.http.get(BASE_URL)
-   } 
-   
+  ngOnInit() {}
+ 
+ 
+  getMyAllData() {
+    return this.http.get(BASE_URL);
   }
+  
+  
+  updateData(data: any) {
+    const body = {
+      method: 'PUT',
+      body: JSON.stringify({
+        id: 1,
+        title: data.title,
+        body: data.title,
+
+        userId: data.id,
+      }),
+      headers: {
+        'content-type': 'application/json; charset=UTF-8',
+      },
+    };
+
+    return this.http.put('${data.id}', body);
+  }
+}
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,9 +2,12 @@ import { Component, OnInit, TemplateRef  } from '@angular/core';
 import { TitleStrategy } from '@angular/router';
 import { observableToBeFn } from 'rxjs/internal/testing/TestScheduler';
 import { CommonService } from '../common.service';
-import { FormsModule } from '@angular/forms';
+import { FormControl, FormsModule } from '@angular/forms';
+import { FormGroup } from '@angular/forms';
 //import { mbdModalRef } from 'ndb-angular-ui-kit/model,';
 import { BsModalRef, BsModalService  } from 'ngx-bootstrap/modal';
+
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -14,42 +17,79 @@ export class HomeComponent  {
   showFlag = false;
   arrData : any;
   editData: {};
-  modalRef: BsModalRef
- constructor(private dataService : CommonService,private modalService: BsModalService ) {}
- 
- openModalnew(template: TemplateRef<any>) {
-  this.modalRef = this.modalService.show(template);
-  this.modalRef.hide();  
-  console.log(this.modalRef)
+  updateData : {};
+  modalRef: BsModalRef;
+  title: string;
+
+  formData: FormGroup;
+  id = new FormControl('');
+ constructor(private dataService : CommonService,private modalService: BsModalService ) { }
+
+  
+  openModalnew(template: TemplateRef<any>  ) {
+     this.modalRef = this.modalService.show(template);
+    console.log(this.modalRef)
 }
 
- 
- 
+
  showStyle(){
   
   this.showFlag = true
 }
   ngOnInit(): void{
-    this.getValues(); 
-    
-      
-     }
-     getValues(){
-     this.dataService.getMyAllData().subscribe({
-      next: (res) => {
-        this.arrData = res;
-        console.log(this.arrData);
-      }
+    this.dataService.getMyAllData().subscribe(Response=> {
+      this.arrData = Response
     })
-    
-    
+    this.formData  = new FormGroup({
+      title: new FormControl(''),
+      id: new FormControl(),
+    });
+    //this.getValues(); 
   }
-  openModal(Data): void {
-    console.log('data Value', Data)
-    this.editData = Data
-  }
-    }
+
+   
+    
+  openModal(Template: TemplateRef<any>, data:any) {
+      this.title = data.title;
+      this.formData.setValue({
+        title: data.Title,
+        id: data.Id
+      });
+      this.modalRef = this.modalService.show(Template);
+    
+      } 
+  OnClickSubmit(updateData: Object): void {
+     console.log('inside the data',  updateData )
+      this.dataService.updateData( updateData ).subscribe(result=> {
+       console.log('result.>', result)
+      })
+      this.modalService.hide()
+    }  }
+   // deletemodal: void {
+
+    //}
+
+
+    
+
+    
   
+  
+     //getValues(){
+     //this.dataService.getMyAllData().subscribe({
+     // next: (res) => {
+       // this.arrData = res;
+        //console.log(this.arrData);
+      //}
+    //});
+    //}
+ 
+
+
+    //console.log('data Value', Data)
+    //this.editData = Data
+
+
     
   
 
