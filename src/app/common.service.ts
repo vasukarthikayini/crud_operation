b85e5ec0b6ae7cceab8eef9b778f401144bb4609
vasukarthikayini <karthikayini.sv@10decoders.in>
2022-08-22 22:38:11 +0530

    update and  new data delete

--- src/app/common.service.ts
+++ src/app/common.service.ts
@@ -5,10 +5,8 @@ import { Data } from '@angular/router';
 @Injectable({
   providedIn: 'root',
 })
-export class CommonService { 
+export class CommonService {
   constructor(private http: HttpClient) {}
-
-  ngOnInit() {}
 
   getMyAllData() {
     return this.http.get(BASE_URL);
@@ -27,15 +25,14 @@ export class CommonService {
         'content-type': 'application/json; charset=UTF-8',
       },
     };
-    
 
-    return this.http.put(`${BASE_URL}/${data.id}`, body)
+    return this.http.put(`${BASE_URL}/${data.id}`, body);
   }
-  deleteData()(data: any): {
+  deleteData(data: any) {
+    console.log('delete the data', data )
     return this.http.delete(`${BASE_URL}/${data.id}`);
+    //deleteData(data: any): {
+    //return this.http.delete(`${BASE_URL}/${data.id}`, body)
+    //}
   }
-
 }
-
-
- 
